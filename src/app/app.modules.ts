5cf13f9afb83ae4962f2f1783ef5519f1752b371
Daria Matveeva <119957276+DannyOhDanny@users.noreply.github.com>
2023-11-21 00:35:53 +0300

    Feat: product zoom, onMouseMove

--- src/app/app.modules.ts
+++ src/app/app.modules.ts
@@ -1,14 +1,14 @@
-import {NgModule} from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
 import {HttpClient, HttpClientModule,} from "@angular/common/http";
 import {BrowserModule} from '@angular/platform-browser';
 import {AppComponent} from "./app.component";
 import {ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
+  declarations: [AppComponent],
   imports: [HttpClientModule, BrowserModule, ReactiveFormsModule],
   providers: [],
-  declarations: [AppComponent],
-  bootstrap: [AppComponent],
+  bootstrap: [AppComponent]
 })
 
 export class AppModule {
